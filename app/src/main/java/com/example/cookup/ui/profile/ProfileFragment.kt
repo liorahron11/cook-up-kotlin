239ad58bb360dd7da4dd3cd8f871e1d983597751
liora <liorahron11@gmail.com>
2025-03-20 22:59:51 +0200

    feat(): refactor

--- app/src/main/java/com/example/cookup/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/cookup/ui/profile/ProfileFragment.kt
@@ -1,9 +1,16 @@
 package com.example.cookup.ui.profile
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -12,24 +19,40 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.target.Target
 import com.example.cookup.R
+import com.example.cookup.models.Comment
+import com.example.cookup.models.Ingredient
+import com.example.cookup.models.Recipe
 import com.example.cookup.models.User
 import com.example.cookup.room.entities.Profile
+import com.example.cookup.room.entities.RecipeEntity
 import com.example.cookup.view_models.AuthViewModel
 import com.example.cookup.room.view_models.ProfileViewModel
 import com.example.cookup.room.view_models.RecipeViewModel
+import com.example.cookup.services.FirestoreService
+import com.google.firebase.Timestamp
+import com.google.gson.Gson
 import java.util.concurrent.Executors
 import kotlin.getValue
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private lateinit var progressBar: ProgressBar
+    private lateinit var noRecipes: RelativeLayout
+    private lateinit var profileLayout: LinearLayout
+    private lateinit var recipesCount: TextView
+    private lateinit var user: User
+    private lateinit var userRecipes: List<Recipe>
     private val authViewModel: AuthViewModel by viewModels()
     private val profileViewModel: ProfileViewModel by viewModels()
+    private val recipeViewModel: RecipeViewModel by viewModels()
     private var cachedImagePath: String? = null
     private val args: ProfileFragmentArgs by navArgs()
-    private lateinit var user: User
     private var cachedProfile: Profile? = null
+    private val firestoreService = FirestoreService()
+    private val gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         if (args.user != null) {
             user = args.user!!
         } else {
@@ -66,6 +89,10 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         super.onViewCreated(view, savedInstanceState)
         setUserDetails()
 
+        recipesCount = view.findViewById(R.id.recipesCount)
+        noRecipes = view.findViewById(R.id.noRecipes)
+        progressBar = view.findViewById(R.id.progressBar)
+        profileLayout = view.findViewById(R.id.profileLayout)
         val settingButton = view.findViewById<Button>(R.id.settingButton)
         settingButton.setOnClickListener {
             findNavController().navigate(R.id.action_profileFragment_to_ProfileSettingsFragment)
@@ -77,10 +104,17 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
             }
         }
 
-        childFragmentManager.beginTransaction()
-            .replace(R.id.gridFragmentContainer, RecipeGridFragment())
-            .commit()
-
+        setLoading(true)
+        recipeViewModel.getRecipesBySenderId(authViewModel.user.value?.uid.toString()) { recipes ->
+            if (recipes.isNotEmpty()) {
+                userRecipes = parseCachedRecipes(recipes)
+                setRecipesCount()
+                openRecipeGridFragment()
+                setLoading(false)
+            } else {
+                fetchDataFromFirestore()
+            }
+        }
     }
 
     private fun setUserDetails() {
@@ -99,5 +133,116 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
                     .into(profileImageView!!)
             }
         }
+    }
+
+    private fun openRecipeGridFragment() {
+        val bundle = Bundle()
+        bundle.putString("recipesJson", Gson().toJson(userRecipes))
+
+        val recipeGridFragment = RecipeGridFragment()
+        recipeGridFragment.arguments = bundle
+
+        childFragmentManager.beginTransaction()
+            .replace(R.id.gridFragmentContainer, recipeGridFragment)
+            .commit()
+    }
+
+    private fun fetchDataFromFirestore() {
+        firestoreService.getUserRecipe(
+            cachedProfile?.id.toString(), onSuccess = { recipes ->
+                if (!recipes.isEmpty()) {
+                    cacheRecipes(recipes)
+                    openRecipeGridFragment()
+                } else {
+                    showNoRecipesMessage(true)
+                }
+
+                userRecipes = recipes
+                setRecipesCount()
+                setLoading(false)
+            },
+            onFailure = { error ->
+                Toast.makeText(requireContext(), "שגיאה", Toast.LENGTH_SHORT).show()
+                setLoading(false)
+            }
+        )
+    }
+    private fun cacheRecipes(recipes: List<Recipe>) {
+        for (recipe in recipes) {
+            var recipeToCache = RecipeEntity(
+                id = recipe.id,
+                timestamp = recipe.timestamp.toDate().time,
+                senderId = recipe.senderId,
+                title = recipe.title,
+                description = recipe.description,
+                instructions = recipe.instructions,
+                ingredients = Gson().toJson(recipe.ingredients),
+                comments = Gson().toJson(recipe.comments),
+                likes = Gson().toJson(recipe.likes),
+                image = recipe.image
+            )
+
+            recipeViewModel.insertRecipe(recipeToCache)
+        }
+    }
+
+    private fun parseCachedRecipes(recipes: List<RecipeEntity>): List<Recipe> {
+        val recipesToReturn = mutableListOf<Recipe>()
+
+        for (recipe in recipes) {
+            recipesToReturn.add(
+                Recipe(
+                    id = recipe.id,
+                    timestamp = Timestamp(recipe.timestamp / 1000, (recipe.timestamp % 1000).toInt()),
+                    senderId = recipe.senderId,
+                    title = recipe.title,
+                    description = recipe.description,
+                    instructions = recipe.instructions,
+                    ingredients = gson.fromJson(recipe.ingredients, Array<Ingredient>::class.java).toList(),
+                    comments = gson.fromJson(recipe.comments, Array<Comment>::class.java).toList(),
+                    likes = gson.fromJson(recipe.likes, Array<String>::class.java).toList(),
+                    image = recipe.image
+                )
+            )
+        }
+
+        return recipesToReturn
+    }
+
+    private fun setLoading(value: Boolean) {
+        if (value) {
+            progressBar.visibility = View.VISIBLE
+            profileLayout.visibility = View.GONE
+        } else {
+            progressBar.visibility = View.GONE
+            profileLayout.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showNoRecipesMessage(value: Boolean) {
+        if (value) {
+            val margin = dpToPx(60, requireContext())
+
+            val params = noRecipes.layoutParams as ViewGroup.MarginLayoutParams
+            params.setMargins(0, margin, 0, 0)
+            noRecipes.layoutParams = params
+            noRecipes.visibility = View.VISIBLE
+            view?.findViewById<FrameLayout>(R.id.gridFragmentContainer)?.visibility = View.GONE
+        } else {
+            val params = noRecipes.layoutParams as ViewGroup.MarginLayoutParams
+            params.setMargins(0, 0, 0, 0)
+            noRecipes.layoutParams = params
+            noRecipes.visibility = View.GONE
+            view?.findViewById<FrameLayout>(R.id.gridFragmentContainer)?.visibility = View.VISIBLE
+
+        }
+    }
+
+    fun dpToPx(dp: Int, context: Context): Int {
+        return (dp * context.resources.displayMetrics.density).toInt()
+    }
+
+    private fun setRecipesCount() {
+        recipesCount.text = userRecipes.size.toString() + " מתכונים"
     }
 }
--- app/src/main/java/com/example/cookup/ui/profile/RecipeGridFragment.kt
+++ app/src/main/java/com/example/cookup/ui/profile/RecipeGridFragment.kt
@@ -1,38 +1,22 @@
 package com.example.cookup.ui.profile
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
-import android.widget.RelativeLayout
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookup.R
-import com.example.cookup.models.Comment
-import com.example.cookup.models.Ingredient
 import com.example.cookup.models.Recipe
-import com.example.cookup.room.entities.RecipeEntity
-import com.example.cookup.room.view_models.RecipeViewModel
-import com.example.cookup.services.FirestoreService
-import com.example.cookup.view_models.AuthViewModel
-import com.google.firebase.Timestamp
 
 class RecipeGridFragment : Fragment() {
     private val gson = Gson()
     private lateinit var recyclerView: RecyclerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var noRecipes: RelativeLayout
-    private lateinit var authViewModel: AuthViewModel
-    private lateinit var recipeViewModel: RecipeViewModel
+    private lateinit var recipes: List<Recipe>
     private var adapter: RecipeAdapter? = null
-    private val firestoreService = FirestoreService()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -42,133 +26,23 @@ class RecipeGridFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        noRecipes = view.findViewById(R.id.noRecipes)
-        progressBar = view.findViewById(R.id.progressBar)
-        recyclerView = view.findViewById(R.id.recipeRecyclerView)
         recyclerView = view.findViewById(R.id.recipeRecyclerView)
         recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
         recyclerView.setHasFixedSize(true)
-        authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
-        recipeViewModel = ViewModelProvider(this)[RecipeViewModel::class.java]
 
-        setLoading(true)
-
-        recipeViewModel.getRecipesBySenderId(authViewModel.user.value?.uid.toString()) { recipes ->
+        arguments?.getString("recipesJson")?.let { json ->
+            recipes = gson.fromJson(json, Array<Recipe>::class.java).toList()
             if (recipes.isNotEmpty()) {
-                adapter = RecipeAdapter(parseCachedRecipes(recipes)) { recipe ->
+                adapter = RecipeAdapter(recipes) { recipe ->
                     navigateToRecipeDetails(recipe)
                 }
                 recyclerView.adapter = adapter
-                setLoading(false)
-            } else {
-                fetchDataFromFirestore()
             }
         }
-    }
-
-    private fun setLoading(value: Boolean) {
-        if (value) {
-            progressBar.visibility = View.VISIBLE
-            recyclerView.visibility = View.GONE
-        } else {
-            progressBar.visibility = View.GONE
-            recyclerView.visibility = View.VISIBLE
-
-        }
-    }
-
-    private fun showNoRecipesMessage(value: Boolean) {
-        if (value) {
-            val margin = dpToPx(60, requireContext())
-
-            val params = noRecipes.layoutParams as ViewGroup.MarginLayoutParams
-            params.setMargins(0, margin, 0, 0)
-            noRecipes.layoutParams = params
-            noRecipes.visibility = View.VISIBLE
-            recyclerView.visibility = View.GONE
-        } else {
-            val params = noRecipes.layoutParams as ViewGroup.MarginLayoutParams
-            params.setMargins(0, 0, 0, 0)
-            noRecipes.layoutParams = params
-            noRecipes.visibility = View.GONE
-            recyclerView.visibility = View.VISIBLE
-
-        }
-    }
-
-    fun dpToPx(dp: Int, context: Context): Int {
-        return (dp * context.resources.displayMetrics.density).toInt()
     }
 
     fun navigateToRecipeDetails(recipe: Recipe) {
         val action = ProfileFragmentDirections.actionProfileFragmentToRecipeDetailsFragment(recipe)
         findNavController().navigate(action)
-    }
-
-    private fun cacheRecipes(recipes: List<Recipe>) {
-        for (recipe in recipes) {
-            var recipeToCache = RecipeEntity(
-                id = recipe.id,
-                timestamp = recipe.timestamp.toDate().time,
-                senderId = recipe.senderId,
-                title = recipe.title,
-                description = recipe.description,
-                instructions = recipe.instructions,
-                ingredients = Gson().toJson(recipe.ingredients),
-                comments = Gson().toJson(recipe.comments),
-                likes = Gson().toJson(recipe.likes),
-                image = recipe.image
-            )
-
-            recipeViewModel.insertRecipe(recipeToCache)
-        }
-    }
-
-    private fun parseCachedRecipes(recipes: List<RecipeEntity>): List<Recipe> {
-        val recipesToReturn = mutableListOf<Recipe>()
-
-        for (recipe in recipes) {
-            recipesToReturn.add(
-                Recipe(
-                    id = recipe.id,
-                    timestamp = Timestamp(recipe.timestamp / 1000, (recipe.timestamp % 1000).toInt()),
-                    senderId = recipe.senderId,
-                    title = recipe.title,
-                    description = recipe.description,
-                    instructions = recipe.instructions,
-                    ingredients = gson.fromJson(recipe.ingredients, Array<Ingredient>::class.java).toList(),
-                    comments = gson.fromJson(recipe.comments, Array<Comment>::class.java).toList(),
-                    likes = gson.fromJson(recipe.likes, Array<String>::class.java).toList(),
-                    image = recipe.image
-                )
-            )
-        }
-
-        return recipesToReturn
-    }
-
-    private fun fetchDataFromFirestore() {
-        firestoreService.getUserRecipe(
-            authViewModel.user.value?.uid.toString(), onSuccess = { recipes ->
-                if (recipes.isEmpty()) {
-                    showNoRecipesMessage(true)
-                } else {
-                    cacheRecipes(recipes)
-
-                    adapter = RecipeAdapter(recipes) { recipe ->
-                        navigateToRecipeDetails(recipe)
-                    }
-                    recyclerView.adapter = adapter
-                    showNoRecipesMessage(false)
-                }
-
-                setLoading(false)
-            },
-            onFailure = { error ->
-                Toast.makeText(requireContext(), "שגיאה", Toast.LENGTH_SHORT).show()
-                setLoading(false)
-            }
-        )
     }
 }
